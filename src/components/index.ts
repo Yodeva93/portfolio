978d65a74e208583295ba4cae94a434003c15196
Yodeva93 <amitthakurwoods@gmail.com>
2026-04-15 15:43:52 +0530

    updated 2

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,7 @@
 // @ts-nocheck
 import {
-  EarthCanvas,
   BallCanvas,
   ComputersCanvas,
-  StarsCanvas,
 } from "./canvas";
 import Hero from "./sections/Hero";
 import Navbar from "./layout/Navbar";
@@ -11,8 +9,6 @@ import About from "./sections/About";
 import Tech from "./sections/Tech";
 import Experience from "./sections/Experience";
 import Works from "./sections/Works";
-import Feedbacks from "./sections/Feedbacks";
-import Contact from "./sections/Contact";
 import CanvasLoader from "./layout/Loader";
 
 export {
@@ -22,11 +18,7 @@ export {
   Tech,
   Experience,
   Works,
-  Feedbacks,
-  Contact,
   CanvasLoader,
-  EarthCanvas,
   BallCanvas,
   ComputersCanvas,
-  StarsCanvas,
 };
