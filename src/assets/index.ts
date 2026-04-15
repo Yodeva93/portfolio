1c49a7ed21cf1a0e52a13cf4b2da6b6da3584117
Yodeva93 <amitthakurwoods@gmail.com>
2026-04-15 16:43:43 +0530

    updated 2.3

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -6,7 +6,6 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import shopifyBadge from "./shopify-development-fundamentals-verified-skill-bad.png";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
